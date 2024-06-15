20dbee146badd6cf67e748fb0ad224a07e51331c
hd900415 <hd900415@gmail.com>
2024-06-15 21:50:57 +0400

    modified:   Docker/install_docker.sh 	modified:   scripts/docker_install_mysql_redis_minio/docker_install_mysql_redis_minio_openresty.yaml 	modified:   scripts/docker_install_mysql_redis_minio/redis/redis.conf

--- Docker/install_docker.sh
+++ Docker/install_docker.sh
@@ -11,6 +11,7 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
 sudo systemctl start docker
 systemctl enable --now docker 
-
+systemctl daemon-reload 
+sudo systemctl restart docker
 # docker compose new 
 docker compose 
