ecb33d265f9a05ad1b896f29636e4894f9da7fc2
scott <scott@1024.tt>
2024-08-02 18:10:11 +0700

    deleted:    Docker/mysql/Dockerfile 	deleted:    Docker/mysql/docker-compose.yml 	modified:   Docker/mysql/use_docker_create_mysql.sh 	new file:   middleware/docker_install_mysql.sh 	new file:   middleware/mysql8conf/my.cnf

--- Docker/mysql/use_docker_create_mysql.sh
+++ Docker/mysql/use_docker_create_mysql.sh
@@ -19,3 +19,14 @@ docker run -d --name mysql8_nacos \
     -v /data/mysql/conf:/etc/mysql/conf.d \
     -e TZ=Asia/Shanghai \
     mysql:8.0.35
+
+
+docker run -d --name mysql \
+--restart always  \
+-p 3306:3306 \
+-e MYSQL_ROOT_PASSWORD='Dtixn$U@xdljtS$3145DF' \
+-e MYSQL_DATABASE='zhijia' \
+-v /etc/localtime:/etc/localtime \
+-v /data/docker/mysql/data:/var/lib/mysql \
+-v /data/docker/mysql/conf/my.cnf:/etc/mysql/conf.d/my.cnf \
+mysql:8.0.35
