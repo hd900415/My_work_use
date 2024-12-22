a64bbe3a9810d7b40b48bbf09a2c8acc2028ae00
hd900415 <hd900415@gmail.com>
2024-12-22 13:22:15 +0800

    modified:   middleware/openresty/build/start.sh

--- middleware/openresty/build/start.sh
+++ middleware/openresty/build/start.sh
@@ -12,3 +12,12 @@ docker run -d --name nginx -p 80:80 -p 88:88 -p 443:443 \
   -v /data/docker/openresty/logs:/usr/local/openresty/nginx/logs \
   -v /data/docker/openresty/www:/var/www/html:ro \
   openresty:1.25.3.1v1 
+
+  docker run -d --name nginx \
+  -p 80:80 -p 88:88 -p 443:443   \
+  --restart always   \
+  -v /etc/localtime:/etc/localtime   \
+  -v /data/docker/openresty/conf:/usr/local/openresty/nginx/conf:ro   \
+  -v /data/docker/openresty/logs:/usr/local/openresty/nginx/logs   \
+  -v /data/docker/openresty/www:/var/www/html:ro  \
+  haidao900/openresty:1.25.3.1v1
