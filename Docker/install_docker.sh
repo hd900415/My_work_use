ab5f5f32b74ed952a9679818a349573d958ef223
hd900415 <hd900415@gmail.com>
2025-02-01 14:08:37 +0800

    deleted:    .idea/.gitignore 	deleted:    .idea/My_work_use.iml 	deleted:    .idea/modules.xml 	deleted:    .idea/vcs.xml 	modified:   Docker/install_docker.sh 	modified:   k8s/nacos/nacos-deploy.yaml 	new file:   k8s/nacos/nacos-ingress.yaml 	modified:   k8s/nacos/nacos-lb.yaml

--- Docker/install_docker.sh
+++ Docker/install_docker.sh
@@ -8,10 +8,29 @@ sudo dnf remove docker \
                   docker-engine
 sudo dnf install -y yum-utils
 sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo docker compose
+
+cat <EOF >/etc/docker/daemon.json
+{
+  "userns-remap": "default",      // 启用用户命名空间隔离
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "10m",
+    "max-file": "3"
+  },
+  "default-ulimits": {
+    "nofile": {
+      "Name": "nofile",
+      "Hard": 65535,
+      "Soft": 20000
+    }
+  },
+  "live-restore": true,          // 保持容器在Docker重启后存活
+  "icc": false                   // 禁用容器间网络通信（默认）
+}
+EOF
 sudo systemctl start docker
-systemctl enable --now docker 
-systemctl daemon-reload 
+systemctl enable --now docker
+systemctl daemon-reload
 sudo systemctl restart docker
-# docker compose new 
-sudo docker compose 
