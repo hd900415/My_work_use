4eb7b819234f43f1f6c7d6f4bdf34c8287d13cf6
hd900415 <hd900415@gmail.com>
2025-02-01 23:01:37 +0800

    new file:   k8s/busybox/nginx-svc.yaml 	modified:   k8s/mysql/configmap.yaml 	modified:   k8s/mysql/mysql-config.cnf 	modified:   k8s/mysql/start.sh

--- k8s/mysql/start.sh
+++ k8s/mysql/start.sh
@@ -4,6 +4,8 @@ cat <<EOF > mysql-config.cnf
 EOF
 > EOF
 kubectl create configmap mysql-config --from-file=mysql-config.cnf -n pt-dev
+
+
 helm install mysql8 -f values.yaml -n pt-dev bitnami/mysql
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
