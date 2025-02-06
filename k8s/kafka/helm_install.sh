749f7deff31077b4f6b95181325e5f30f851dca3
jenkins <hd900415@gmail.com>
2025-02-06 19:51:14 +0800

    new file:   k8s/elk/canal/canal-adapter-cm-es.yaml 	modified:   k8s/elk/canal/canal-adapter-cm.yaml 	modified:   k8s/elk/canal/canal-adapter.yaml 	modified:   k8s/jenkins/jenkinsfile/pt-admin/pt-admin.jenkinsfile 	new file:   k8s/kafka/helm_install.sh 	new file:   k8s/kafka/kafka-cm.yaml 	new file:   k8s/kafka/kafka-sts.yaml 	new file:   k8s/kafka/kafka-svc.yaml

---
+++ k8s/kafka/helm_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm pull oci://registry-1.docker.io/bitnamicharts/kafka --version 31.3.1
+
+
+
+# 默认使用kraft mode
+
+helm upgrade --install canal-kafka --namespace es  bitnami/kafka --version 31.3.1 \
+--set replicaCount=3 \
+--set global.defaultStorageClass="nfs-storageclass" \
+--set kraft.enabled=true 
