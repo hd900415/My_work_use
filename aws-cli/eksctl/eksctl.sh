fd616531b4029e6c7341dc4b750ae4bf456ca947
jenkins <hd900415@gmail.com>
2025-02-17 18:24:51 +0800

    modified:   aws-cli/eksctl/eksctl.sh 	new file:   k8s/ws-gateway/centrifugal/centrifugo-cm.yaml 	new file:   k8s/ws-gateway/centrifugal/centrifugo-deploy.yaml 	new file:   k8s/ws-gateway/centrifugal/centrifugo-ingress.yaml 	new file:   k8s/ws-gateway/centrifugal/centrifugo-svc.yaml 	new file:   k8s/zentao/zentao-deploy.yaml

--- aws-cli/eksctl/eksctl.sh
+++ aws-cli/eksctl/eksctl.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # 创建EKS集群
-eksctl create cluster --name new-eks-cluster --region ap-east-1 \
-  --version 1.31 \ # 指定Kubernetes版本
-  --vpc-nat-mode HighlyAvailable \  # 启用多可用区 NAT 网关
-  --zones ap-east-1a,ap-east-1b,ap-east-1c \
-  --without-nodegroup  # 先仅创建 VPC
+eksctl create cluster --name=eks-cluster --region=ap-east-1 --vpc-cidr=10.0.0.0/16 --zones=ap-east-1a,ap-east-1b,ap-east-1c --nodegroup-name=eks-cluster-ng-1 --node-type=m5.large --nodes=2 --nodes-min=2 --nodes-max=4 --node-volume-size=20 --node-volume-type=gp3 --ssh-access --ssh-public
 # 创建nodegroup
 eksctl create nodegroup --cluster=eks-cluster-hk --region=ap-east-1 \
   --name=eks-cluster-hk-ng-1 \
@@ -23,7 +19,8 @@ eksctl create nodegroup --cluster=eks-cluster-hk --region=ap-east-1 \
   --full-ecr-access \
   --alb-ingress-access \
   --install-neuron-plugin=false \
-  --install-nvidia-plugin=false 
+  --install-nvidia-plugin=false \
+  --subnet-ids subnet-009210ff2c60cd7d3,subnet-0a421392815f6cda3,subnet-09336a4348acdd256
 
 # 查看集群状态
 eksctl get cluster --name=elk-hk-1-31-cluster
