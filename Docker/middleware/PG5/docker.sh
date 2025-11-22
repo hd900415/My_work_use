6e249786ff59f8c38bf88d3699fd4a258874644e
阿飞 <afe@9pt.me>
2025-11-23 01:52:39 +0800

    add file

--- Docker/middleware/PG5/docker.sh
+++ Docker/middleware/PG5/docker.sh
@@ -11,3 +11,32 @@ docker run -d --name xboard-pg \
   postgres:15
 
   docker exec -it xboard-pg psql -U postgres -d xboard -c "\l"
+
+docker run -itd --restart=always  --network=host \
+ -e apiHost=https://adminxboard.pttech.cc \
+ -e apiKey=FY4DYhoHUfxB4qG2Sqo3 \
+ -e domain=hy2.pttech.cc  \
+ -e nodeID=1 \
+ghcr.io/cedar2025/hysteria:latest
+
+
+
+v2board:
+  apiHost: https://adminxboard.pttech.cc
+  apiKey: FY4DYhoHUfxB4qG2Sqo3
+  nodeID: 1
+tls:
+  type: tls
+  cert: /etc/hysteria/tls.crt
+  key: /etc/hysteria/tls.key
+auth:
+  type: v2board
+trafficStats:
+  listen: 127.0.0.1:7653
+acl: 
+  inline: 
+    - reject(10.0.0.0/8)
+    - reject(172.16.0.0/12)
+    - reject(192.168.0.0/16)
+    - reject(127.0.0.0/8)
+    - reject(fc00::/7)
